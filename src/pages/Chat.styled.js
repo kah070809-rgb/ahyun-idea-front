d6292eef8bfa71f118c341d16185634684fae77e
ahyun <kah070809@gmail.com>
2026-05-15 15:25:57 +0900

    다시

--- src/pages/Chat.styled.js
+++ src/pages/Chat.styled.js
@@ -7,7 +7,7 @@ export const wavePulse = keyframes`
 
 export const Container = styled.div`
     width: 100%;
-    max-width: 430px;
+    max-width: 420px;
     margin: 0 auto;
     height: 100dvh;
     background-color: #1a1a2e;
